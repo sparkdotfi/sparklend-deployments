df0d50f3a37f3c199214b6c1e460e390a7a03e17
alphak3y <alphak3y@protonmail.com>
2025-03-07 07:01:21 -0700

    M-06 initial supply goes to address zero

--- src/periphery/contracts/misc/ReserveInitializer.sol
+++ src/periphery/contracts/misc/ReserveInitializer.sol
@@ -52,11 +52,11 @@ contract ReserveInitializer is Ownable {
                     // For HYPE tokens, wrap them to WHYPE first
                     WRAPPED_TOKEN_GATEWAY.depositHYPE{value: msg.value}(address(POOL), msg.sender, 0);
                 } else {
-                    require(IERC20(underlyingAsset).balanceOf(msg.sender) >= initialAmounts[i], string(abi.encodePacked("Insufficient balance of ", IERC20Metadata(underlyingAsset).symbol())));
+                    require(IERC20(underlyingAsset).balanceOf(address(this)) >= initialAmounts[i], string(abi.encodePacked("Insufficient balance of ", IERC20Metadata(underlyingAsset).symbol())));
                     // Approve pool to spend tokens
                     IERC20(underlyingAsset).safeIncreaseAllowance(address(POOL), initialAmounts[i]);
                     // Supply to pool
-                    POOL.supply(underlyingAsset, initialAmounts[i], msg.sender, 0);
+                    POOL.supply(underlyingAsset, initialAmounts[i], address(0), 0);
                 }
             }
         }
