3be67a2b25206746d966d2b6f02cccb934882d3d
Mike Albanese <alphak3y@protonmail.com>
2025-05-01 12:00:45 -0600

    cleanup bindings contracts

--- src/bindings/IHyFiPool.sol
+++ src/bindings/IHyFiPool.sol
@@ -8,34 +8,4 @@ import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
  * @author HypurrFi
  * @notice Interface for the HyFi pool, extending Aave's pool interface
  */
-interface IHyFiPool is IPool {
-    /**
-     * @dev Emitted when a borrow occurs
-     * @param user The address of the user initiating the borrow
-     * @param asset The address of the asset being borrowed
-     * @param amount The amount being borrowed
-     * @param interestRateMode The interest rate mode (stable or variable)
-     * @param borrowRate The borrow rate at the moment of the borrow
-     */
-    event Borrow(
-        address indexed user,
-        address indexed asset,
-        uint256 amount,
-        uint256 interestRateMode,
-        uint256 borrowRate
-    );
-
-    /**
-     * @dev Emitted when a supply occurs
-     * @param user The address of the user initiating the supply
-     * @param asset The address of the asset being supplied
-     * @param amount The amount being supplied
-     * @param referralCode The referral code used
-     */
-    event Supply(
-        address indexed user,
-        address indexed asset,
-        uint256 amount,
-        uint16 referralCode
-    );
-}
+interface IHyFiPool is IPool {}
